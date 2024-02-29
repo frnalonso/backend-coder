37f68764ab1c22276bb50a3ce75c5e12fb2e7d5e
frnalonso <fnalonso7@gmail.com>
2024-02-29 16:43:16 -0300

    #commit 3

--- src/router/products.router.js
+++ src/router/products.router.js
@@ -17,10 +17,10 @@ router.get('/', async(req,res)=>{
     }
 })
 
-router.get('/:idProduct',async(req,res)=>{
+router.get('/:pid',async(req,res)=>{
     try {
-        const {idProduct} = req.params
-        const product = await productManager.getProdctById(+idProduct);
+        const {pid} = req.params
+        const product = await productManager.getProdctById(+pid);
         if (!product) {
             res.status(400).json({message: 'Productos no encontrado con el id ingresado'})
             console.log("producto no encontrado")
@@ -36,24 +36,24 @@ router.get('/:idProduct',async(req,res)=>{
 router.post('/', async(req,res)=>{
     try {
         console.log(req.body)
-       const newProduct = await productManager.createProduct(req.body);
+       const newProduct = await productManager.addProduct(req.body);
         res.status(200).json({message: 'Producto agregado satisfactoriamente...', product: newProduct })
     } catch (error) {
         res.status(400).json({message:error})
     }
 })
 
-router.put('/:idProduct', async(req,res)=>{
-    const {idProduct} = req.params
+router.put('/:pid', async(req,res)=>{
+    const {pid} = req.params
    
     try {
-        const response = await productManager.updateProduct(+idProduct,req.body)
+        const response = await productManager.updateProduct(+pid,req.body)
 
         if (response === -1) {
             res.status(400).json({message: 'Producto no encontrado con el id ingresado'})
         } else {
             
-            await productManager.updateProduct(+idProduct);
+            await productManager.updateProduct(+pid);
             res.status(200).json({message: 'Producto modificado satisfactoriamente...'})
         }
 
@@ -62,10 +62,10 @@ router.put('/:idProduct', async(req,res)=>{
     }
 })
 
-router.delete('/:idProduct',async(req,res)=> {
-const {idProduct} = req.params
+router.delete('/:pid',async(req,res)=> {
+const {pid} = req.params
     try {
-        await productManager.deleteProduct(+idProduct)
+        await productManager.deleteProduct(+pid)
         res.status(200).json({message: 'Producto eliminado correctamente...'})
     } catch (error) {
         res.status(500).json({message: error})
